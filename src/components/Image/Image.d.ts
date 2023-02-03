0b8177da67653c50cb8bce5ed436b40ac35ed191
Zied Hafoudhi <Zied.Hafoudhi@opal-rt.com>
2023-02-02 20:55:28 -0500

    Change loading props name to loadingNode so we can use the loadding image attribute

--- src/components/Image/Image.d.ts
+++ src/components/Image/Image.d.ts
@@ -1,22 +1,23 @@
 import { ReactNode } from "react";
 
-export interface ImageProps extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'loading'> {
-    animationDuration?: number,
-    aspectRatio?: number,
-    cover?: boolean,
-    color?: string,
-    disableError?: boolean,
-    disableSpinner?: boolean,
-    disableTransition?: boolean,
-    errorIcon?: ReactNode,
-    iconContainerStyle?: object,
-    imageStyle?: object,
-    loading?: ReactNode,
-    onClick?: (event: React.MouseEvent<HTMLDivElement>) => void,
-    onLoad?: (event?: React.SyntheticEvent<HTMLImageElement>) => void,
-    onError?: (event?: React.SyntheticEvent<HTMLImageElement>) => void,
-    src: string,
-    style?: object
+export interface ImageProps
+  extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, "loading"> {
+  animationDuration?: number;
+  aspectRatio?: number;
+  cover?: boolean;
+  color?: string;
+  disableError?: boolean;
+  disableSpinner?: boolean;
+  disableTransition?: boolean;
+  errorIcon?: ReactNode;
+  iconContainerStyle?: object;
+  imageStyle?: object;
+  loadingNode?: ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onLoad?: (event?: React.SyntheticEvent<HTMLImageElement>) => void;
+  onError?: (event?: React.SyntheticEvent<HTMLImageElement>) => void;
+  src: string;
+  style?: object;
 }
 
 declare const ImageComponent: React.ComponentType<ImageProps>;
